72430f92c140bfcd6663d9ada8b24a4a2b34854b
Sergio Durán <sergio@tiene.info>
2024-04-18 11:21:01 -0600

    Arreglar el calculo de daño min/max

--- src/routes/ViewBattle.svelte
+++ src/routes/ViewBattle.svelte
@@ -169,7 +169,7 @@ let log:string[] = []
 			{#each Champs as source}
 				{#each Champs as target}
 					{@const damage = calculateDamage(source, target)}
-					{source.name}->{target.name}: {damage.rolls.length}d{damage.sides}({damage.sides}-{damage.max}) (promedio: {(damage.sides+damage.max)/2})<br>
+					{source.name}->{target.name}: {damage.rolls.length}d{damage.sides}({damage.rolls.length}-{damage.max}) (promedio: {(damage.rolls.length+damage.max)/2})<br>
 				{/each}
 				<br>
 			{/each}
