181d3dcb3f859f88838a93c41650249f08268def
Sergio Durán <sergio@tiene.info>
2024-04-22 04:53:20 -0600

    Mejorar el estilo de la tarjeta de unidad

--- src/lib/Unit.svelte
+++ src/lib/Unit.svelte
@@ -4,33 +4,34 @@
 
 	export let champ:Champ
 </script>
-<table class="table table-sm mb-0">
-	<tr>
-		<td>
-		<b>Def:</b>
-		</td>
-		<td>{champ.defense} </td>
-		<td>
-		<b>Vel:</b> 
-		</td>
-		<td>{champ.movespeed} </td>
-	</tr>
-	<tr>
-		<td>
-		<b>Elemento:</b>
-		</td>
-		<td>
-			<ElementIcon element={champ.element} /> 
-		</td>
-	</tr>
-</table>
-		<b>Objetivos:</b> <br>
-		&nbsp;{champ.targetting.name}<br>
-		<b>Dados de ataque:</b><br>
 <div class="row">
+
+	<div class="col-12 position-relative">
+		<b>Elemento:</b> 
+		<span class="position-absolute end-0 me-4">
+			<ElementIcon element={champ.element} /> 
+		</span>
+	</div>
+	<div class="col-6 position-relative">
+		<b>Def:</b> 
+		<span class="position-absolute end-0 me-4">{champ.defense}</span>
+	</div>
+	<div class="col-6">
+		<b>Vel:</b>
+		<span class="position-absolute end-0 me-4">{champ.movespeed}</span>
+	</div>
+	<div class="col-12">
+		<b>Objetivos:</b> <br>
+		<span class="ms-4">
+		{champ.targetting.name}<br>
+		</span>
+		<b>Dados de ataque:</b><br>
+	</div>
 	{#each champ.elementStrength.sort((a,b) => b.dice-a.dice) as es}
 	<div class="col-6">
-		<ElementIcon element={es.element} /> {es.dice}d{es.sides}+{es.modifier}<br>
+		<span class="ms-4">
+			<ElementIcon element={es.element} /> {es.dice}d{es.sides}+{es.modifier}<br>
+		</span>
 	</div>
 	{/each}
 </div>
