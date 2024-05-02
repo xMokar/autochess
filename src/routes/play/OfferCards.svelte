3d169a5325221e2b0ab9e96211d58fe606fc260f
Sergio Durán <sergio@tiene.info>
2024-05-02 10:33:19 -0600

    Cambiar el color del boton de comprar

--- src/routes/play/OfferCards.svelte
+++ src/routes/play/OfferCards.svelte
@@ -16,7 +16,7 @@ Oro: {player.gold} Cartas en la mano: {player.hand.length}<br>
 	{#each offered as unit, i}
 		<div class="col-12 col-md-4 mb-1">
 			{#snippet actions()}
-					<button disabled={player.gold==0} class="btn btn-sm btn-danger" onclick={() => buy(i)} title="Comprar"><span class="bi bi-currency-dollar"></span></button>
+					<button disabled={player.gold==0} class="btn btn-sm btn-warning" onclick={() => buy(i)} title="Comprar"><span class="bi bi-currency-dollar"></span></button>
 			{/snippet}
 			<UnitCard {unit} {actions} />
 		</div>
