639859bfe42c9986c424b4293deaffb5c56d651f
Sergio Durán <sergio@tiene.info>
2024-05-03 08:32:10 -0600

    Esconder las cartas en la tienda por privacidad

--- src/routes/play/Shop.svelte
+++ src/routes/play/Shop.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
     import UnitCard from "$lib/UnitCard.svelte";
     import type { Player, Unit } from "$lib/system";
+    import Page from "../+page.svelte";
     import Hand from "./Hand.svelte";
 
 let {cards, player = $bindable(), oncontinue, ontake}:{
@@ -19,11 +20,13 @@ let buy = (index:number) => {
 	player.hand.push(ontake(index))
 }
 let _oncontinue = () => {
+	show = false
 	oncontinue()
 }
 let viewhand = () => {
 	view="hand"
 }
+let show = $state(false)
 </script>
 
 {#if view=="shop"}
@@ -31,6 +34,9 @@ let viewhand = () => {
 	<div class="card-header">
 		Tienda para <span class="text-{player.color}">{player.name}</span>
 		<span class="float-end">
+			{#if !show}
+				<button class="btn btn-outline-primary" onclick={() => show = true}>Mostrar cartas</button>
+			{/if}
 			<button onclick={viewhand} class="btn btn-outline-secondary">Ver mano</button>
 
 			<button onclick={_oncontinue} class="btn btn-outline-danger">Continuar</button>
@@ -39,14 +45,24 @@ let viewhand = () => {
 	<div class="card-body">
 		Oro: {player.gold} Cartas en la mano: {player.hand.length}<br>
 		<div class="row mt-2">
-			{#each cards as unit, i}
-				<div class="col-12 col-md-4 mb-1">
-					{#snippet actions()}
-							<button disabled={player.gold==0} class="btn btn-sm btn-warning" onclick={() => buy(i)} title="Comprar"><span class="bi bi-currency-dollar"></span></button>
-					{/snippet}
-					<UnitCard {unit} {actions} />
-				</div>
-			{/each}
+				{#each cards as unit, i}
+					<div class="col-12 col-md-4 mb-1">
+						{#if show}
+							{#snippet actions()}
+									<button disabled={player.gold==0} class="btn btn-sm btn-warning" onclick={() => buy(i)} title="Comprar"><span class="bi bi-currency-dollar"></span></button>
+							{/snippet}
+							<UnitCard {unit} {actions} />
+						{:else}
+							<div class="card">
+								<div class="card-header">Carta escondida</div>
+								<div class="card-body">
+									El contenido de esta carta esta oculto por privacidad, puedes darle click al boton de Mostrar para ver tus cartas.
+								</div>
+							</div>
+							
+						{/if}
+					</div>
+				{/each}
 		</div>
 
 	</div>
