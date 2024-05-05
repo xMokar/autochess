d5e46bbbfaa428161c43076664ca7588fe09eab5
Sergio Durán <sergio@tiene.info>
2024-05-05 10:14:00 -0600

    Refactorizar el codigo de la tienda

--- src/routes/play/Shop.svelte
+++ src/routes/play/Shop.svelte
@@ -3,6 +3,7 @@
     import type { Player, Unit } from "$lib/system";
     import Page from "../+page.svelte";
     import Hand from "./Hand.svelte";
+    import ShopContents from "./ShopContents.svelte";
 
 let {cards, player = $bindable(), oncontinue, ontake}:{
 		cards:Unit[],
@@ -13,7 +14,7 @@ let {cards, player = $bindable(), oncontinue, ontake}:{
 
 let view = $state("shop")
 
-let buy = (index:number) => {
+let onbuy = (index:number) => {
 	if(player.gold==0)
 		return;
 	player.gold--
@@ -30,46 +31,17 @@ let show = $state(false)
 </script>
 
 {#if view=="shop"}
-<div class="card mt-2">
-	<div class="card-header">
-		Tienda para <span class="text-{player.color}">{player.name}</span>
-		<span class="float-end">
-			{#if !show}
-				<button class="btn btn-outline-primary" onclick={() => show = true}>Mostrar cartas</button>
-			{/if}
-			<button onclick={viewhand} class="btn btn-outline-secondary">Ver mano</button>
+{#snippet buttons()}
+	{#if !show}
+		<button class="btn btn-outline-primary" onclick={() => show = true}>Mostrar cartas</button>
+	{/if}
+	<button onclick={viewhand} class="btn btn-outline-secondary">Ver mano</button>
 
-			<button onclick={_oncontinue} class="btn btn-outline-danger">Siguiente jugador</button>
-		</span>
-	</div>
-	<div class="card-body">
-		Oro: {player.gold} Cartas en la mano: {player.hand.length}<br>
-		<div class="row mt-2">
-				{#each cards as unit, i}
-					<div class="col-12 col-md-4 mb-1">
-						{#if show}
-							{#snippet actions()}
-									<button disabled={player.gold==0} class="btn btn-sm btn-warning" onclick={() => buy(i)} title="Comprar"><span class="bi bi-currency-dollar"></span></button>
-							{/snippet}
-							<UnitCard {unit} {actions} />
-						{:else}
-							<div class="card">
-								<div class="card-header">Carta escondida</div>
-								<div class="card-body">
-									El contenido de esta carta esta oculto por privacidad, si eres <span class="text-{player.color}">{player.name}</span> puedes darle click al boton de Mostrar para ver tus cartas.
-								</div>
-							</div>
-							
-						{/if}
-					</div>
-				{/each}
-		</div>
-
-	</div>
-</div>
+	<button onclick={_oncontinue} class="btn btn-outline-danger">Siguiente jugador</button>
+{/snippet}
+	<ShopContents {player} {buttons} {cards} {show} {onbuy} />
 {:else}
 	<Hand {player} onclose={() => view="shop"} cardActions={undefined} boardActions={undefined} closeText="Regresar" />
-
 {/if}
 		<p>
 			<b>Instrucciones:</b><br>
---
+++ src/routes/play/ShopContents.svelte
@@ -0,0 +1,45 @@
+<script lang="ts">
+    import UnitCard from '$lib/UnitCard.svelte';
+    import type { Player, Unit } from '$lib/system';
+    import type { Snippet } from 'svelte';
+
+let { player, buttons, cards, show, onbuy }:{
+	player:Player,
+	buttons:Snippet,
+	cards:Unit[],
+	onbuy:(i:number) => void,
+	show:boolean
+} = $props()
+</script>
+<div class="card mt-2">
+	<div class="card-header">
+		Tienda para <span class="text-{player.color}">{player.name}</span>
+		<span class="float-end">
+			{@render buttons()}
+		</span>
+	</div>
+	<div class="card-body">
+		Oro: {player.gold} Cartas en la mano: {player.hand.length}<br>
+		<div class="row mt-2">
+				{#each cards as unit, i}
+					<div class="col-12 col-md-4 mb-1">
+						{#if show}
+							{#snippet actions()}
+								<button disabled={player.gold==0} class="btn btn-sm btn-warning" onclick={() => onbuy(i)} title="Comprar"><span class="bi bi-currency-dollar"></span></button>
+							{/snippet}
+							<UnitCard {unit} {actions} />
+						{:else}
+							<div class="card">
+								<div class="card-header">Carta escondida</div>
+								<div class="card-body">
+									El contenido de esta carta esta oculto por privacidad, si eres <span class="text-{player.color}">{player.name}</span> puedes darle click al boton de Mostrar para ver tus cartas.
+								</div>
+							</div>
+							
+						{/if}
+					</div>
+				{/each}
+		</div>
+
+	</div>
+</div>
