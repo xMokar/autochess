ef0cbd4bdba39c0f1d6fded1a07298bca26dd728
Sergio Durán <sergio@tiene.info>
2024-05-08 11:47:39 -0600

    Cambiar el estilo de los rasgos

--- src/lib/Effect.svelte
+++ src/lib/Effect.svelte
@@ -3,7 +3,23 @@ import type { Effect } from "./system";
 
 let { effect }:{effect:Effect} = $props();
 </script>
-<span class="badge me-1" class:bg-success={effect.active} class:bg-secondary={!effect.active} style="white-space: nowrap">{@html effect.message}</span>
+<style>
+.effect {
+background: #fff;
+border: 1px solid #ccc;
+border-radius: 5px;
+}
+
+.active {
+border: 2px solid #000;
+font-weight: bold;
+background: #afa;
+}
+
+</style>
+<span class="d-inline-block text-dark effect me-1 mt-1" class:active={effect.active} class:inactive={!effect.active}>
+		{@html effect.message}
+</span>
 
 
 
