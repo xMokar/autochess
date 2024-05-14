1a4daea81e949609c69fd8c1c2d3f8ff8b0f915a
Sergio Durán <sergio@tiene.info>
2024-05-13 20:58:06 -0600

    Mejorar la vista de unidades aún más

--- src/lib/UnitCard.svelte
+++ src/lib/UnitCard.svelte
@@ -3,7 +3,6 @@
     import Modal from "./Modal.svelte";
     import TraitIcon from "./TraitIcon.svelte";
     import UnitInfo from "./UnitInfo.svelte";
-    import UnitTraits from "./UnitTraits.svelte";
 
 	let { unit, actions = undefined, onclick, field }:{
 		unit:Unit, 
@@ -24,19 +23,19 @@
 {/snippet}
 <Modal bind:show={showModal} body={card} />
 <div class="card w-100">
-	<div class="card-header">
+	<div class="card-header p-0 ps-2">
 		{unit.name}
 		<div class="float-end">
 			<button onclick={() => showModal=true} class="btn btn-sm btn-secondary"><span class="bi bi-info-circle-fill"></span></button>
 		</div>
 	</div>
-	<div class="card-body p-0" style="height: 150px">
+	<div class="card-body p-0" style="height: 120px">
 
 		<div class="unit position-relative" {onclick} role="button">
 			<img src="/units/{unit.id}.png" width="100%" class="position-absolute {unit.id}" alt={unit.name} />
 			<div class="overlay position-absolute bottom-0 pb-2 ps-2">
 				{#each unit.traits as trait}
-					<div class="trait mb-1 pe-3">
+					<div class="trait mb-1 pe-3" style="font-size: 80%">
 						<TraitIcon {trait} /> {trait.name}<br>
 					</div>
 				{/each}
@@ -51,7 +50,7 @@
 
 <style>
 .unit {
-  height: 150px;
+  height: 120px;
   width: 100%;
   overflow: hidden;
 }
--- src/routes/play/DropUnitCard.svelte
+++ src/routes/play/DropUnitCard.svelte
@@ -7,8 +7,8 @@ let { onclick, unit }:{
 }= $props()
 </script>
 <div class="card w-100">
-	<div class="card-header">&nbsp;</div>
-	<div class="card-body p-0" style="height: 150px">
+	<div class="card-header p-0 ps-2">&nbsp;</div>
+	<div class="card-body p-0" style="height: 120px">
 		<button {onclick} class="btn btn-sm btn-primary h-100 w-100">Soltar {unit.name} aquí</button>
 	</div>
 </div>
