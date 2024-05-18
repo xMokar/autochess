9880a2aa2154025b919561510197028c94d3bf2f
Sergio Durán <sergio@tiene.info>
2024-05-17 20:50:24 -0600

    Mejorar el estilo de los rasgos de equipo

--- src/routes/play/TraitInfo.svelte
+++ src/routes/play/TraitInfo.svelte
@@ -19,7 +19,7 @@ let modal = $state(false)
 {/snippet}
 {#snippet info()}
 	<div class="card">
-		<div class="card-header">Rasgo activo</div>
+		<div class="card-header"><TraitIcon trait={trait.trait} /> {trait.trait.name} {trait.active}/{trait.levels[trait.level].amount}</div>
 		<div class="card-body">
 			{#each trait.effects as effect}
 				<TraitIcon trait={effect.target} /> {@render type(effect.type)}: {effect.value>0? '+'+effect.value:effect.value}
@@ -32,4 +32,4 @@ let modal = $state(false)
 <Modal onclose={() => modal = false} body={info} />
 {/if}
 <button class="btn btn-secondary btn-sm p-0" onclick={()=> modal=true}><span class="bi bi-info-circle-fill"></span></button>
-{trait.trait.name}: {trait.active}/{trait.levels[trait.level].amount}
+<TraitIcon trait={trait.trait} /> {trait.trait.name} {trait.active}/{trait.levels[trait.level].amount}
